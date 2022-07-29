22b2ee58daf408652ec23142ff61d41570ae6a0d
Kenneth Mckrola <kmckrola@dapdash.com>
2022-07-29 14:21:03 -0600

    api key hide

--- app/Services/RetrieveGoogleReviewsService.php
+++ app/Services/RetrieveGoogleReviewsService.php
@@ -7,11 +7,12 @@ class RetrieveGoogleReviewService
      */
     public static function fetchAll()
     {
+        $google_key = getenv('GOOGLE_API_KEY');
         $businesses = Business::whereNotNull('google_place_id')->get();
         foreach($businesses as $biz)
         {
             $pid = $biz->google_place_id;
-           //$url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJwWJZjhLwsgIRWC4gaAJyfSM&key=AIzaSyCpnTgq66VsUxzBbwlAaK3pRTIQ8qq9SBw"
+           //$url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJwWJZjhLwsgIRWC4gaAJyfSM&key=".$google_key;
         }
     }
 }
